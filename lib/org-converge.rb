2c2ecb0d0ea5397d4703230c31ff89a8614c6d11
Waldemar Quevedo <waldemar.quevedo@gmail.com>
2014-03-23 21:23:04 +0900

    Add BabelOutputBuffer

--- lib/org-converge.rb
+++ lib/org-converge.rb
@@ -1,2 +1,36 @@
-require "org-converge/version"
-require "org-converge/command"
+require 'org-ruby'
+require 'org-converge/babel_output_buffer'
+require 'org-converge/command'
+require 'org-converge/version'
+
+module Orgmode
+  class Parser
+
+    # This would return a babel output buffer which has the methods
+    # needed in order to be able to tangle the files
+    def babelize
+      # Feed the parsed contens and create the necessary internal structures
+      # for doing babel like features
+      output = ''
+      ob = BabelOutputBuffer.new(output)
+      translate(@header_lines, ob)
+      @headlines.each do |headline|
+        translate(headline.body_lines, ob)
+      end
+
+      ob
+    end
+  end
+end
+
+module StringWithColors
+  def red;    colorize("\e[0m\e[31m");  end
+  def green;  colorize("\e[0m\e[32m");  end
+  def yellow; colorize("\e[0m\e[33m");  end
+  def bold;   colorize("\e[0m\e[1m");   end
+  def colorize(color_code); "#{color_code}#{self}\e[0m"; end
+end
+
+class String
+  include StringWithColors
+end
--- lib/org-converge/command.rb
+++ lib/org-converge/command.rb
@@ -1,13 +1,33 @@
-require 'org-ruby'
-
 module OrgConverge
   class Command
+    attr_reader :dotorg
+
     def initialize(options)
       @options = options
+      @dotorg  = options['<org_file>']
     end
 
     def execute!
-      puts "TODO"
+      case
+      when @options['--showfiles']
+        showfiles
+      end      
+    end
+
+    def showfiles
+      ob = Orgmode::Parser.new(File.read(dotorg)).babelize
+
+      ob.tangle.each do |file, lines|
+        puts "---------- #{file} --------------".green
+        lines.each do |line|
+          puts line
+        end
+      end
+
+      ob.scripts.each_with_index do |script, index|
+        puts "---------- script: #{index} --------------".green
+        puts script
+      end
     end
   end
 end
