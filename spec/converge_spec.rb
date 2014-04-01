36444613d9c64ff0d0f1b61e6454ab07cd2777f9
Waldemar Quevedo <waldemar.quevedo@gmail.com>
2014-04-01 21:37:46 +0900

    Add test for org run

--- spec/converge_spec.rb
+++ spec/converge_spec.rb
@@ -23,6 +23,20 @@ describe OrgConverge::Command do
       result.should == expected_contents
     end
   end
+
+  context "when converging 'basic_run_example'" do
+    example_dir = File.join(EXAMPLES_DIR, 'basic_run_example')
+    setup_file = File.join(example_dir, 'setup.org')
+    o = OrgConverge::Command.new({ 
+                                   '<org_file>' => setup_file,
+                                   '--root-dir' => example_dir
+                                 })
+    success = o.execute!
+    success.should == true
+
+    expected_contents = File.read(File.join(example_dir, 'out.log'))
+    expected_contents.lines.count.should == 16
+  end
 end
 
 
