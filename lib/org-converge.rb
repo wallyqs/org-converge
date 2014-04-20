ebf1b54482f42f96d58f214827ee3e086598b685
Waldemar Quevedo <waldemar.quevedo@gmail.com>
2014-04-20 14:24:55 +0900

    Remove old coloring code since we use tco gem now

--- lib/org-converge.rb
+++ lib/org-converge.rb
@@ -24,21 +24,3 @@ module Orgmode
     end
   end
 end
-
-module StringWithColors
-  def red;            colorize("\e[0m\e[31m");  end
-  def green;          colorize("\e[0m\e[32m");  end
-  def yellow;         colorize("\e[0m\e[33m");  end
-  def blue;           colorize("\e[0m\e[34m");  end
-  def magenta;        colorize("\e[0m\e[35m"); end
-  def cyan;           colorize("\e[0m\e[36m"); end
-  def white;          colorize("\e[0m\e[37m"); end
-  def bright_black;   colorize("\e[0m\e[30m"); end
-  def bright_magenta; colorize("\e[0m\e[35m"); end
-  def bold;   colorize("\e[0m\e[1m");   end
-  def colorize(color_code); "#{color_code}#{self}\e[0m"; end
-end
-
-class String
-  include StringWithColors
-end
